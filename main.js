31dcea683aeb2e4cf427c8841ed41d2bb26c98f5
YashGupta3 <y.yashgupta03@gmail.com>
2023-03-26 20:08:10 +0530

    reloading problem solved

--- main.js
+++ main.js
@@ -1,5 +1,50 @@
 let form = document.getElementById("booking-form");
+
 form.addEventListener("submit", addToCloud);
+
+window.addEventListener("DOMContentLoaded", () => {
+  axios
+    .get(
+      "https://crudcrud.com/api/2d1f52ce6deb492fa8f1250a34962399/bookingData"
+    )
+    .then((res) => {
+      for (let i = 0; i < res.data.length; i++) {
+        let newElement = document.createElement("li");
+        let newBtn = document.createElement("input");
+        let editBtn = document.createElement("input");
+        newBtn.className = "delete";
+        newBtn.type = "button";
+        newBtn.value = "Delete";
+        editBtn.className = "edit";
+        editBtn.type = "button";
+        editBtn.value = "Edit";
+
+        newElement.appendChild(
+          document.createTextNode(JSON.stringify(res.data[i]))
+        );
+        // newBtn.appendChild(document.createTextNode('Delete'))
+
+        let parentEle = document.getElementById("list");
+        parentEle.appendChild(newElement);
+        newElement.appendChild(newBtn);
+        newElement.appendChild(editBtn);
+        newBtn.onclick = () => {
+          // localStorage.removeItem(obj.email);
+          parentEle.removeChild(newElement);
+        };
+        editBtn.onclick = () => {
+          // localStorage.removeItem(obj.email);
+          parentEle.removeChild(newElement);
+          document.getElementById("username").value = obj.uname;
+          document.getElementById("email").value = obj.email;
+          document.getElementById("phone").value = obj.phone;
+        };
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 function addToCloud(e) {
   e.preventDefault();
@@ -19,33 +64,36 @@ function addToCloud(e) {
     )
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
+  displayData();
 
-  let newElement = document.createElement("li");
-  let newBtn = document.createElement("input");
-  let editBtn = document.createElement("input");
-  newBtn.className = "delete";
-  newBtn.type = "button";
-  newBtn.value = "Delete";
-  editBtn.className = "edit";
-  editBtn.type = "button";
-  editBtn.value = "Edit";
+  function displayData() {
+    let newElement = document.createElement("li");
+    let newBtn = document.createElement("input");
+    let editBtn = document.createElement("input");
+    newBtn.className = "delete";
+    newBtn.type = "button";
+    newBtn.value = "Delete";
+    editBtn.className = "edit";
+    editBtn.type = "button";
+    editBtn.value = "Edit";
 
-  newElement.appendChild(document.createTextNode(JSON.stringify(obj)));
-  // newBtn.appendChild(document.createTextNode('Delete'))
+    newElement.appendChild(document.createTextNode(JSON.stringify(obj)));
+    // newBtn.appendChild(document.createTextNode('Delete'))
 
-  let parentEle = document.getElementById("list");
-  parentEle.appendChild(newElement);
-  newElement.appendChild(newBtn);
-  newElement.appendChild(editBtn);
-  newBtn.onclick = () => {
-    // localStorage.removeItem(obj.email);
-    parentEle.removeChild(newElement);
-  };
-  editBtn.onclick = () => {
-    // localStorage.removeItem(obj.email);
-    parentEle.removeChild(newElement);
-    document.getElementById("username").value = obj.uname;
-    document.getElementById("email").value = obj.email;
-    document.getElementById("phone").value = obj.phone;
-  };
+    let parentEle = document.getElementById("list");
+    parentEle.appendChild(newElement);
+    newElement.appendChild(newBtn);
+    newElement.appendChild(editBtn);
+    newBtn.onclick = () => {
+      // localStorage.removeItem(obj.email);
+      parentEle.removeChild(newElement);
+    };
+    editBtn.onclick = () => {
+      // localStorage.removeItem(obj.email);
+      parentEle.removeChild(newElement);
+      document.getElementById("username").value = obj.uname;
+      document.getElementById("email").value = obj.email;
+      document.getElementById("phone").value = obj.phone;
+    };
+  }
 }
